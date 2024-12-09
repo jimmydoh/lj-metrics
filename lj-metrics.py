4db884bec424cb870856018375162bf6f870adf5
jimmydoh <jimmydoh@gmail.com>
2024-12-09 15:42:38 +1000

    Create lj-metrics.py

---
+++ lj-metrics.py
@@ -0,0 +1,28 @@
+import datetime
+import os
+from time import sleep
+
+from ping3 import ping
+
+INTERVAL = 30
+DESTINATION = "1.1.1.1"
+
+count = 0
+header = f"Pinging {DESTINATION} every {INTERVAL} secs"
+print(header)
+
+while True:
+    count += 1
+    latency = ping(DESTINATION)
+
+    if latency is None:
+        latency_text = "PACKET DROPPED"
+    elif latency is False:
+        latency_text = "NO CONNECTION"
+    else:
+        latency_text = f"{latency} secs"
+
+    line = f"{datetime.datetime.now()}: pinged {DESTINATION}; latency: {latency_text}"
+    print(f"{line}")
+  
+    sleep(INTERVAL)
